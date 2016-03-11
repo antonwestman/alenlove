b74a338ccecd68a496e387ee090acd5855dd6bef
Anton Westman <anton.westman@apoex.se>
2016-03-11 22:21:55 +0100

    Move OSA and populate email

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('alenlove.controllers', [])
 
 .controller('HomeCtrl', function() {})
 
-.controller('RSVPCtrl', function() {
+.controller('RSVPCtrl', function($window, $state) {
   vm = this
   vm.form = {};
 
@@ -14,7 +14,15 @@ angular.module('alenlove.controllers', [])
     };
   };
 
-  vm.send = function() {};
+  vm.send = function() {
+    var subject = "Bröllop - "+vm.form.name+ (vm.form.attending ? " kommer" : " kommer inte");
+    var foodPrefString = "Matpreferenser: "+ (vm.form.foodPref ? vm.form.foodPref : "Inga");
+    var attendString = vm.form.attending ? "vill självklart" : "kan tyvärr inte komma och";
+    var busString = vm.form.bus ? "vill gärna ha" : "tar mig dit på egen hand och behöver alltså inte"
+    var body = "Hej blivande brudpar.%0D%0A%0D%0AJag "+attendString+" fira er stora dag.%0D%0AJag "+busString+" en plats i bussen.%0D%0A%0D%0A"+foodPrefString
+    $window.open('mailto:brollop@alenlov.se?subject='+subject+'&body='+body);
+    $state.go("tab.home")
+  };
 })
 
 .controller('TransportCtrl', function() {})
